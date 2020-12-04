eabdcff4f42be808e86f2d6c67a342e6f8c7ef91
EduardOST <eduard.ortega@boomlabs.edu.co>
2020-12-04 15:32:17 -0500

    creando aplicacion en code

---
+++ proyectof/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace proyectof.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
