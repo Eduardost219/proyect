1c99e437ebba591ac842bcd022b76b60115e4db0
EduardOST <eduard.ortega@boomlabs.edu.co>
2020-12-06 21:01:52 -0500

    creando el server, mongoDB y agregando bootstrap

--- proyectof/index.js
+++ proyectof/index.js
@@ -1,22 +1,35 @@
 const express = require('express')
-const app = express();
- //settings 
-app.set('port',4000);
-
-//middlewares 
-
-
-//routes 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+const app = express()
+const mongoose = require('mongoose');
+ 
+//conectar a mongodb
+mongoose.connect('mongodb+srv://user:contraseñasegura@democluster.itjs7.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
 });
 
 
+const Cat = mongoose.model('Cat', { name: String });
 
-//static files
+const kitty = new Cat({ name: 'ALAN' });
+kitty.save().then(() => console.log('meow'));
 
 
- // escuchando el servidor 
-app.listen(app.get('port'), () => {
-     console.log('server on port', app.get('port'))
-});
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.get('/Contacto', function (req, res) {
+  res.send('Esta es la página de contacto')
+})
+
+app.get('/Pelusas', function (req, res) {
+  app.get('/', function (req, res) {
+    const kitty = new Cat({ name: 'ALAN' });
+    kitty.save().then(() => console.log('meow'));
+})
+})
+ 
+app.listen(3000)
